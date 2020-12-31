23586e0231b563cfebe6d5322cbd8ddf18f05cd4
virsantillan1 <virsantillan98@hotmail.com.ar>
2020-12-31 10:31:13 -0300

    correciones

---
+++ app/build/generated/source/buildConfig/debug/com/virsantillan1/holamundo/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.virsantillan1.holamundo;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.virsantillan1.holamundo";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
